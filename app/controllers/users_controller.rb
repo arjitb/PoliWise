1a1f85f7e71f388bf5a447f165e3de460d640935
Amirah Chamble <amirah.chamble@yahoo.com>
2016-04-05 21:31:10 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) #not final implementation
       if @user.save
-        #succesfully render
+        flash[:success] = "Welcome to PoliWise!"
+        redirect_to @user
       else
         render 'new'
       end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,6 +23,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | The Sample App"
+    assert_select "title", "Contact | PoliWise"
   end
 end
